051df9e2f7e279f7a291eb0ff1de651c77572b3f
Muhammad Dio Tsani Iba - 24 <dio.tsani123@gmail.com>
2022-08-24 09:45:42 +0800

    update grid detection

--- Assets/Scripts/Scene/Gameplay/PlayingGrid.cs
+++ Assets/Scripts/Scene/Gameplay/PlayingGrid.cs
@@ -6,46 +6,61 @@ public class PlayingGrid : MonoBehaviour
 {
     int _height = 8;
     int _width = 8;
-    public float _gridSpace = 1f;
+    private float _gridSpace = 1f;
     [SerializeField] private GameObject gridPrefab;
     public Vector3 gridOrigin = Vector3.zero;
-    private GameObject[,] gridSize;
-    private List<GameObject> gridObject;
+    [SerializeField] private List<GameObject> gridList = new List<GameObject>();
+    public int _amountColorOne;
+    private void Awake()
+    {
+        CreateGrid();
+    }
     void Start()
     {
-        SpawnGrid();
+        //OnHitPlayerOne();
         
     }
     void Update()
     {
-        
+        CalculateGridColor();
     }
 
     private void CreateGrid()
-    {
-        gridSize = new GameObject[_height, _width];
-        if(gridPrefab == null)
-        {
-            Debug.Log("Empty");
-            return;
-        }
-    }
-
-    private void SpawnGrid()
     {
         for (int x = 0; x < _height; x++)
         {
             for (int z = 0; z < _width; z++)
             {
-
                 Vector3 spawnPosition = new Vector3(x * _gridSpace, 0, z * _gridSpace) + gridOrigin;
                 Spawn(spawnPosition, Quaternion.identity);
             }
         }
     }
-
     void Spawn(Vector3 positionToSpawn, Quaternion rotationToSpawn)
     {
-        GameObject gridObjects = Instantiate(gridPrefab, positionToSpawn, rotationToSpawn,transform);
+        GameObject gridObjects = Instantiate(gridPrefab, positionToSpawn, rotationToSpawn, transform);
+        gridList.Add(gridObjects);
+    }
+    void OnHitPlayerOne() // Need Event On Trigger in Script Player
+    {
+        foreach (GameObject grid_1 in gridList)
+        {
+            grid_1.GetComponent<MeshRenderer>().material.color = Color.red;
+        }
+    }
+    void CalculateGridColor() // Need Event On Trigger with Item collect Point
+    {
+        foreach (GameObject gridColors in gridList)
+        {
+            for (int i = 0; i < gridList.Count; i++)
+            {
+                if (gridColors.GetComponent<MeshRenderer>().material.color == Color.red)
+                {
+                    _amountColorOne = gridList.Count;
+                    Debug.Log(gridList.Count);
+                }
+            }
+            
+        }
     }
 }
