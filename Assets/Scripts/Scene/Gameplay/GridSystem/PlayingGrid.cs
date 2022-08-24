37afecc28afd976d84d79d8c93746135b4ecaf9e
Muhammad Dio Tsani Iba - 24 <dio.tsani123@gmail.com>
2022-08-24 15:38:50 +0800

    update grid colors

---
+++ Assets/Scripts/Scene/Gameplay/GridSystem/PlayingGrid.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PaintAstic.Global;
+
+public class PlayingGrid : MonoBehaviour
+{
+    int _height = 8;
+    int _width = 8;
+    private float _gridSpace = 1f;
+    [SerializeField] private Tile gridPrefab;
+    public Vector3 gridOrigin = Vector3.zero;
+    private List<Tile> gridList = new List<Tile>();
+
+    public int _amountColorOne;
+    public int _amountColorTwo;
+    private Color defaultColor = Color.gray;
+    private void Awake()
+    {
+        CreateGrid();
+    }
+    private void CreateGrid()
+    {
+        for (int x = 0; x < _height; x++)
+        {
+            for (int z = 0; z < _width; z++)
+            {
+                Vector3 spawnPosition = new Vector3(x * _gridSpace, 0, z * _gridSpace) + gridOrigin;
+                Spawn(spawnPosition, Quaternion.identity);
+            }
+        }
+    }
+    void Spawn(Vector3 positionToSpawn, Quaternion rotationToSpawn)
+    {
+        Tile gridObjects = Instantiate(gridPrefab, positionToSpawn, rotationToSpawn, transform);
+        gridObjects.GetComponent<Tile>().DefaultColors();
+        gridList.Add(gridObjects);
+    }
+    public void OnHitPlayerOne(GameObject obj) // Need Event On Trigger in Script Player
+    {
+        if (obj.GetComponent<MeshRenderer>().material.color == defaultColor)
+        {
+            _amountColorOne += 1;
+            obj.gameObject.GetComponent<MeshRenderer>().material.color = Color.yellow;
+        }
+
+        if (obj.GetComponent<MeshRenderer>().material.color == Color.red)
+        {
+            _amountColorOne += 1;
+            _amountColorTwo -= 1;
+            obj.gameObject.GetComponent<MeshRenderer>().material.color = Color.yellow;
+        }
+    }
+    public void OnHitPlayerTwo(GameObject obj) // Need Event On Trigger in Script Player
+    {
+        if (obj.GetComponent<MeshRenderer>().material.color == defaultColor)
+        {
+            _amountColorTwo += 1;
+            obj.gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
+        }
+
+        if (obj.GetComponent<MeshRenderer>().material.color == Color.yellow)
+        {
+            _amountColorOne -= 1;
+            _amountColorTwo += 1;
+            obj.gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
+        }
+    }
+    public void OnHitPlayerOneRender(Renderer obj) // Need Event On Trigger in Script Player
+    {
+        if (obj.material.color == defaultColor)
+        {
+            _amountColorOne += 1;
+            obj.material.color = Color.yellow;
+        }
+
+        if (obj.material.color == Color.red)
+        {
+            _amountColorOne += 1;
+            _amountColorTwo -= 1;
+            obj.material.color = Color.yellow;
+        }
+    }
+    public void OnHitPlayerTwoRender(Renderer obj) // Need Event On Trigger in Script Player
+    {
+        if (obj.material.color == defaultColor)
+        {
+            _amountColorTwo += 1;
+            obj.material.color = Color.red;
+        }
+
+        if (obj.material.color == Color.yellow)
+        {
+            _amountColorOne -= 1;
+            _amountColorTwo += 1;
+            obj.material.color = Color.red;
+        }
+    }
+}
---
+++ Assets/Scripts/Scene/Gameplay/Tile.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PaintAstic.Global;
+
+public class Tile : MonoBehaviour
+{
+    private Color defaultColor = Color.gray;
+    [SerializeField]private Color[] playerColor = { Color.red, Color.yellow };
+    void Start()
+    {
+    }
+    private void OnEnable()
+    {
+        EventManager.StartListening("SetColor", ChangeColors);
+    }
+    private void OnDisable()
+    {
+        EventManager.StopListening("SetColor", ChangeColors);
+    }
+    public void DefaultColors()
+    {
+        gameObject.GetComponent<Renderer>().material.color = defaultColor;
+    }
+
+    private void ChangeColors(object indexPlayer)
+    {
+        int colorIndex = (int)indexPlayer;
+        gameObject.GetComponent<Renderer>().material.color = playerColor[colorIndex];
+    }
+
+}
