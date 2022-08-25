8993c1b4af47f516ac59b6fd612457ffa9a57734
raihanmaziz <raihan441@gmail.com>
2022-08-25 10:38:33 +0800

    Add particle spawner(still not working)

---
+++ Assets/Scripts/Scene/Gameplay/ParticleSpawner/ParticleSpawner.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PaintAstic.Scene.Gameplay.ParticleSpawner
+{
+    public class ParticleSpawner : MonoBehaviour
+    {
+
+    }
+}
+
