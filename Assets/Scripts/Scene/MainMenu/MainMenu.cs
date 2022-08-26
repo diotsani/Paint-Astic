a1419faf28209c6225bd0fde3b6197bee87dc2d9
Muhamad Faisal <61342260+Icankkkk@users.noreply.github.com>
2022-08-26 13:49:16 +0800

    Update Setting
    Add IButtonAble

--- Assets/Scripts/Scene/MainMenu/MainMenu.cs
+++ Assets/Scripts/Scene/MainMenu/MainMenu.cs
@@ -2,46 +2,56 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Events;
 using UnityEngine.SceneManagement;
+using PaintAstic.Global;
 
 namespace PaintAstic.Scene.MainMenu
 {
-    public class MainMenu : MonoBehaviour
+    public class MainMenu : MonoBehaviour, IButtonAble
     {
         [SerializeField] private Button _playButton;
         [SerializeField] private Button _SettingButton;
         [SerializeField] private Button _ExitButton;
+        [SerializeField] private GameObject _settingPage;
+        [SerializeField] private GameObject _menuPage;
 
         private void Awake()
         {
-            SetPlayButtonListener(OnClickPlayButton);
-            SetSettingButtonListener(OnClickSettingButton);
-            SetExitButtonListener(OnClickExitButton);
+            SetAllButtonListener();
         }
 
         private void SetPlayButtonListener(UnityAction listener) => SetButtonListener(_playButton, listener);
         private void SetSettingButtonListener(UnityAction listener) => SetButtonListener(_SettingButton, listener);
         private void SetExitButtonListener(UnityAction listener) => SetButtonListener(_ExitButton, listener);
 
-        private void SetButtonListener(Button button, UnityAction listener)
+        public void SetButtonListener(Button button, UnityAction listener)
         {
             button.onClick.RemoveAllListeners();
             button.onClick.AddListener(listener);
         }
 
+
         private void OnClickPlayButton()
-        {
-            Debug.Log("Load to gameplay!");
+        {        
             SceneManager.LoadScene("Gameplay");
         }
 
         private void OnClickSettingButton()
-        {
-            Debug.Log("Open setting button!");
+        {         
+            _settingPage.SetActive(true);
+            _menuPage.SetActive(false);
         }
 
         private void OnClickExitButton()
         {
             Debug.Log("Quit game!");
+            Application.Quit();
+        }
+
+        public void SetAllButtonListener()
+        {
+            SetPlayButtonListener(OnClickPlayButton);
+            SetSettingButtonListener(OnClickSettingButton);
+            SetExitButtonListener(OnClickExitButton);
         }
     }
 }
--- Assets/Scripts/Scene/MainMenu/Setting.cs
+++ Assets/Scripts/Scene/MainMenu/Setting.cs
@@ -2,22 +2,56 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using UnityEngine.Events;
+using PaintAstic.Global;
 
 namespace PaintAstic.Scene.MainMenu
 {
-    public class Setting : MonoBehaviour
+    public class Setting : MonoBehaviour, IButtonAble
     {
         [SerializeField] private Button _sfxButton;
         [SerializeField] private Button _bgmButton;
         [SerializeField] private Button _backButton;
         [SerializeField] private TextMeshProUGUI _textSfx;
         [SerializeField] private TextMeshProUGUI _textBgm;
+        [SerializeField] private GameObject _menuPage;
+        [SerializeField] private GameObject _settingPage;
 
-
-        private void BackButtonListener(Button button, UnityAction listener)
+        private void Awake()
         {
+            SetAllButtonListener();
+        }
 
+        private void SetBackButtonListener(UnityAction listener) => SetButtonListener(_backButton, OnClickBackButton);
+
+        public void SetButtonListener(Button button, UnityAction listener)
+        {
+            button.onClick.RemoveAllListeners();
+            button.onClick.AddListener(listener);
+        }
+
+        private void OnClickBackButton()
+        {
+            _settingPage.SetActive(false);
+            _menuPage.SetActive(true);
+        }
+
+
+        private void OnClickSfxButton()
+        {
+            // TODO: @faisal
+        }
+
+        private void OnClickBgmButton()
+        {
+            // TODO: @faisal
+        }
+
+        public void UpdateSfxState(bool isMuted) => _textSfx.SetText($"SFX {(isMuted ? "Off" : "On")}"); 
+        public void UpdateBgmState(bool isMuted) => _textBgm.SetText($"BGM {(isMuted ? "Off" : "On")}"); 
+        
+        public void SetAllButtonListener()
+        {
+            SetBackButtonListener(OnClickBackButton);
         }
     }
-
 }
